9c1917832e3a54943133f23705213cbf76756045
Nick Frichette <frichetten@gmail.com>
2016-08-05 14:36:57 -0500

    Implemented the outline of the create new user button. Put the edit texts in there for it to take user input. Also included the submit button that starts the hashing function. Hashing is SHA-256. I have confirmed that is correct. Considering trying repeated hashing to make it more secure.

---
+++ app/src/main/java/io/github/frichetten/cryptojournal/createNewUser.java
@@ -0,0 +1,51 @@
+package io.github.frichetten.cryptojournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.security.MessageDigest;
+
+public class createNewUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_user);
+
+        final EditText username = (EditText) findViewById(R.id.usernameEditText);
+        username.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+        final EditText password = (EditText) findViewById(R.id.passwordEditText);
+
+        Button createUser = (Button) findViewById(R.id.submitUserButton);
+        createUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String passwd = password.getText().toString();
+
+                //Need to hash the password
+                byte[] test = hashPass("Nick");
+                String output = String.format("%064x", new java.math.BigInteger(1, test));
+                Toast.makeText(getApplicationContext(), user + " " + output, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private static byte[] hashPass(String password){
+        try {
+            java.security.MessageDigest hasher = java.security.MessageDigest.getInstance("SHA-256");
+            hasher.reset();
+            hasher.update(password.getBytes("UTF-8"));
+            return hasher.digest();
+        }
+        catch(java.security.NoSuchAlgorithmException e) { Log.d("Result", "No Such Algorithm Exception"); }
+        catch(java.io.UnsupportedEncodingException e) { Log.d("Result", "Unsupported Encoding Exception"); }
+        return null;
+    }
+}
