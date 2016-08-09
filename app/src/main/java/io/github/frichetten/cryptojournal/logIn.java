ca01c95b023e4e5d8d2f903e18d0da1ee6d4edd6
Nick Frichette <frichetten@gmail.com>
2016-08-08 21:40:42 -0500

    Implemented user list view that reads from storage.txt file. I remember when I struggled to implement a list view in Android. Look at me now! Next step will to be polish the output and make it look a bit better. Then I will have to work on validating that user input as well as developing how and where the user data will be stored. As well as max file size.

--- app/src/main/java/io/github/frichetten/cryptojournal/logIn.java
+++ app/src/main/java/io/github/frichetten/cryptojournal/logIn.java
@@ -1,10 +1,15 @@
 package io.github.frichetten.cryptojournal;
 
+import android.net.wifi.ScanResult;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class logIn extends AppCompatActivity {
 
@@ -13,14 +18,30 @@ public class logIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        String content = "";
+        ListView lv = (ListView) findViewById(R.id.userSelectListView);
+        List<String> userData = readUsers();
+        if (userData.size() == 0)
+            userData.add("NO USERS IN SYSTEM");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                userData
+        );
+        lv.setAdapter(arrayAdapter);
+
+    }
+
+    private ArrayList<String> readUsers(){
+        ArrayList<String> toReturn = new ArrayList<>();
         try {
             FileInputStream fis = openFileInput("Storage.txt");
             byte[] input = new byte[fis.available()];
             while(fis.read(input) != -1)
-                content += new String(input);
-            Log.d("Result", content);
+                toReturn.add(new String(input));
+            //Log.d("Result", content);
         }   catch (java.io.FileNotFoundException e) { Log.d("Result", "File not found"); }
-            catch (java.io.IOException e) { Log.d("Result", "IO Exception"); }
+        catch (java.io.IOException e) { Log.d("Result", "IO Exception"); }
+        return toReturn;
     }
 }
